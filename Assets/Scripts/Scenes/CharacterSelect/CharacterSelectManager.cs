ed7144837c80da61d93a79a6b4ff3531b9bf99c0
jwkim88 <jwkim88@gmail.com>
2015-04-04 22:27:50 -0400

    Local multiplayer hook up preliminary work

--- Assets/Scripts/Scenes/CharacterSelect/CharacterSelectManager.cs
+++ Assets/Scripts/Scenes/CharacterSelect/CharacterSelectManager.cs
@@ -43,18 +43,39 @@ public class CharacterSelectManager : Singleton<CharacterSelectManager>
         base.Awake();
 
 		// Assign Unity UI's event system's default selection, depending on player number
-		switch(PhotonNetwork.player.ID){
-		case 1:
-			LocalEventSystem.firstSelectedGameObject = CharacterButtons[0].gameObject;
-			break;
-		case 2:
-			LocalEventSystem.firstSelectedGameObject = CharacterButtons[1].gameObject;
-			break;
+		switch(Globals.GameMode){
+			case GameModes.ONLINE_MULTIPLAYER:
+				switch(PhotonNetwork.player.ID){
+				case 1:
+					LocalEventSystem.firstSelectedGameObject = CharacterButtons[0].gameObject;
+					break;
+				case 2:
+					LocalEventSystem.firstSelectedGameObject = CharacterButtons[1].gameObject;
+					break;
+				}
+				break;
+			case GameModes.LOCAL_MULTIPLAYER:
+				LocalEventSystem.firstSelectedGameObject = CharacterButtons[0].gameObject;
+				break;
 		}
+//		Globals.HasGameStarted = true;
         cPhotonView = GetComponent<PhotonView>();
     }
 
-	void Update(){
+	private void Start(){
+		switch(Globals.GameMode){
+		case GameModes.ONLINE_MULTIPLAYER:
+			ExecuteEvents.Execute<ISelectHandler>(LocalEventSystem.firstSelectedGameObject, new BaseEventData(EventSystem.current), 
+			                                      ExecuteEvents.selectHandler);
+			break;
+		case GameModes.LOCAL_MULTIPLAYER:
+			ExecuteEvents.Execute<ISelectHandler>(CharacterButtons[0].gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.selectHandler);
+			ExecuteEvents.Execute<ISelectHandler>(CharacterButtons[1].gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.selectHandler);
+			break;
+		}
+	}
+
+	private void Update(){
 		if(Input.GetButtonDown("Cancel")){
 			OnClick_ReturnToMenu();
 		}
@@ -68,51 +89,64 @@ public class CharacterSelectManager : Singleton<CharacterSelectManager>
     {
         currentSelected = _id;
 		Debug.Log ("Current selected is " + currentSelected);
-		cPhotonView.RPC("RPC_SelectCharacter", PhotonTargets.All, PhotonNetwork.player.ID, currentSelected);
+		switch(Globals.GameMode){
+			case GameModes.ONLINE_MULTIPLAYER:
+				cPhotonView.RPC("RPC_SelectCharacter", PhotonTargets.All, PhotonNetwork.player.ID, currentSelected);
+				break;
+			case GameModes.LOCAL_MULTIPLAYER:
+				SelectCharacter(1, currentSelected);
+				break;
+		}
+
     }
 
     public void OnClick_LockCharacter()
     {
-        cPhotonView.RPC("RPC_LockCharacter", PhotonTargets.All, PhotonNetwork.player.ID - 1, currentSelected);
+		switch(Globals.GameMode){
+		case GameModes.ONLINE_MULTIPLAYER:
+			cPhotonView.RPC("RPC_LockCharacter", PhotonTargets.All, PhotonNetwork.player.ID - 1, currentSelected);
+			break;
+		case GameModes.LOCAL_MULTIPLAYER:
+			// but... who pressed it..?
+			LockCharacter(0, currentSelected);
+			break;
+		}
+        
     }
 
 	public void OnClick_ReturnToMenu(){
 		Debug.Log ("Loading previous menu");
 		switch(Globals.GameMode){
 			case GameModes.ONLINE_MULTIPLAYER:
-				Application.LoadLevel("network_lobby");
+				PhotonNetwork.LoadLevel("network_lobby");
 				PhotonNetwork.LeaveRoom();
 				break;
 			default:
-				Application.LoadLevel("main_menu");
+				PhotonNetwork.LoadLevel("main_menu");
 				break;
 
 		}
 	}
 
     #endregion
-
-    #region RPC
-
-    [RPC]
-    private void RPC_SelectCharacter(int _playerNum, int _id)
-    {
+	#region Methods
+	private	void SelectCharacter(int _playerNum, int _id){
 		playersReady[_playerNum - 1] = false;
-        switch (_playerNum)
-        {
-            case 1:
-				p1CurrentSelected = _id;
-				P1Selector.anchoredPosition = new Vector2(CharacterButtons[_id].anchoredPosition.x, P1Selector.anchoredPosition.y) ;
-				P1CharacterPanel.OnCharacterSelect(_id);
-                break;
-
-            case 2:
-				p2CurrentSelected = _id;
-				P2Selector.anchoredPosition = new Vector2(CharacterButtons[_id].anchoredPosition.x, P1Selector.anchoredPosition.y) ;
-				P2CharacterPanel.OnCharacterSelect(_id);
-                break;
-
-        }
+		switch (_playerNum)
+		{
+		case 1:
+			p1CurrentSelected = _id;
+			P1Selector.anchoredPosition = new Vector2(CharacterButtons[_id].anchoredPosition.x, P1Selector.anchoredPosition.y) ;
+			P1CharacterPanel.OnCharacterSelect(_id);
+			break;
+			
+		case 2:
+			p2CurrentSelected = _id;
+			P2Selector.anchoredPosition = new Vector2(CharacterButtons[_id].anchoredPosition.x, P1Selector.anchoredPosition.y) ;
+			P2CharacterPanel.OnCharacterSelect(_id);
+			break;
+			
+		}
 		if(p1CurrentSelected == p2CurrentSelected && DualSelector.GetComponent<Image>().enabled == false){
 			P1Selector.GetComponent<Image>().enabled = false;	
 			P2Selector.GetComponent<Image>().enabled = false;
@@ -123,26 +157,36 @@ public class CharacterSelectManager : Singleton<CharacterSelectManager>
 			P2Selector.GetComponent<Image>().enabled = true;
 			DualSelector.GetComponent<Image>().enabled = false;
 		}
+	}
+
+	private void LockCharacter(int _index, int _id){
+		Globals.SelectedCharacters[_index] = (CharacterID)_id;
+		playersReady[_index] = true;
+		switch(_index){
+		case 0:
+			P1CharacterPanel.OnCharacterConfirm();
+			break;
+		case 1:
+			P2CharacterPanel.OnCharacterConfirm();
+			break;
+		}
+		
+		if (playersReady[0] && playersReady[1] && PhotonNetwork.isMasterClient)
+			PhotonNetwork.LoadLevel(MAP_BEACH);
+	}
+	#endregion
+    #region RPC
+
+    [RPC]
+    private void RPC_SelectCharacter(int _playerNum, int _id)
+    {
+		SelectCharacter(_playerNum, _id);
     }
 
     [RPC]
     private void RPC_LockCharacter(int _index, int _id)
     {
-
-        Globals.SelectedCharacters[_index] = (CharacterID)_id;
-        playersReady[_index] = true;
-		switch(_index){
-			case 0:
-				P1CharacterPanel.OnCharacterConfirm();
-				break;
-			case 1:
-				P2CharacterPanel.OnCharacterConfirm();
-				break;
-		}
-
-
-        if (playersReady[0] && playersReady[1] && PhotonNetwork.isMasterClient)
-            PhotonNetwork.LoadLevel(MAP_BEACH);
+		LockCharacter(_index, _id);
     }
 
     #endregion
