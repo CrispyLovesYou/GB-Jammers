09dda28f3c80b66d16450aba1a65a6e9ab4cf1ac
Crispy <crispylovesyou@live.com>
2015-04-05 16:44:15 -0500

    Fixed a Flag issue. Fixed a Pause issue. Put in some music.

---
+++ Assets/Scripts/Scenes/MainMenu/MainMenuMusic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    #region Fields
+
+    public static MainMenuMusic Instance;
+
+    #endregion
+
+    #region Unity Callbacks
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    #endregion
+}
