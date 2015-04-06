0629e25179a34b15225cea33d4fd5ee10a8dcf7d
Crispy <crispylovesyou@live.com>
2015-04-05 21:44:28 -0500

    Fixed some bugs. Reimported better looping version of gameplay track.

---
+++ Assets/Scripts/Scenes/MainMenu/MusicDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicDestroy : MonoBehaviour
+{
+    private static MusicDestroy Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public static void DestroyObject()
+    {
+        Destroy(Instance.gameObject);
+    }
+}
