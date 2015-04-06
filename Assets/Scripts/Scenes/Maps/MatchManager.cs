6780561307007bb71239a9ad56123938df9f4466
Crispy <crispylovesyou@live.com>
2015-04-05 20:19:45 -0500

    git hub for windows I want to kill you

--- Assets/Scripts/Scenes/Maps/MatchManager.cs
+++ Assets/Scripts/Scenes/Maps/MatchManager.cs
@@ -296,7 +296,7 @@ public class MatchManager : Singleton<MatchManager>
         if (onBeginResetAfterScore != null)
             onBeginResetAfterScore(this, EventArgs.Empty);
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.0f);
 
         if (onCompleteResetAfterScore != null)
             onCompleteResetAfterScore(this, EventArgs.Empty);
